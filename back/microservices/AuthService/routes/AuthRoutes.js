1a41144279752f6cc588a1b5b56ad56498ff70e3
misaelMino <misael.mino@protonmail.com>
2025-06-25 18:32:14 -0300

    add: endpoint /register

--- back/microservices/AuthService/routes/AuthRoutes.js
+++ back/microservices/AuthService/routes/AuthRoutes.js
@@ -2,13 +2,17 @@ import express from 'express';
 import { authenticateUser, authMiddleware } from '../middlewares/authMiddleware.js';
 import { loginController, refreshController } from '../controllers/authController.js';
 import { generateQRController, verifyTOTPController } from '../controllers/totpController.js';
-
+import { registerController } from '../controllers/userController.js';
 const router = express.Router();
 
 router.post('/login', authenticateUser, loginController);
 router.post('/refresh', refreshController);
 router.post('/generate-qr', generateQRController);
 router.post('/verify-totp', authMiddleware, verifyTOTPController);
+router.post('/register', registerController);
+
+
+
 
 
 
