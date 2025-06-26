7c3d0a1ee4f429404bfdc85389b07c1431cc3eac
misaelMino <misael.mino@protonmail.com>
2025-06-25 22:59:16 -0300

    add: queda el micro de Auth terminado s-consigna

--- back/microservices/AuthService/config/configMongo.js
+++ back/microservices/AuthService/config/configMongo.js
@@ -4,15 +4,12 @@ dotenv.config();
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI); // ✅ limpio
     console.log('✅ Conectado a MongoDB');
-  } catch (error) {
-    console.error('❌ Error al conectar a MongoDB:', error);
+  } catch (e) {
+    console.error('❌ Error al conectar:', e.message);
     process.exit(1);
   }
 };
 
-export default connectMongo;
+connectMongo();
